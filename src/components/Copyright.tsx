0c0c29c14dfa20b7a370620518b12a53a377a298
unknown <61201131+kevinCubas@users.noreply.github.com>
2023-05-18 00:30:45 -0300

    feat[refactor]: separate components, and get Github code/token

---
+++ src/components/Copyright.tsx
@@ -0,0 +1,18 @@
+export function Copyright() {
+  return (
+    <footer className="text-sm leading-relaxed text-gray-200">
+      <p>
+        Feito com 💜 por{' '}
+        <a
+          href="https://github.com/kevinCubas"
+          target="_blank"
+          rel="noreferrer"
+          className="underline hover:text-gray-100"
+        >
+          Kevin
+        </a>{' '}
+        durante a NLW
+      </p>
+    </footer>
+  )
+}
