48813b7663de9c7b49387ab92a7b8b9221c293f0
unknown <61201131+kevinCubas@users.noreply.github.com>
2023-05-25 01:37:11 -0300

    build: add remoretePatters

--- next.config.js
+++ next.config.js
@@ -1,7 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ['avatars.githubusercontent.com', 'localhost'],
+    domains: ['avatars.githubusercontent.com'],
+    remotePatterns: [
+      {
+        protocol: 'http',
+        hostname: 'localhost',
+        port: '3333',
+        pathname: '/uploads/**',
+      },
+    ],
   },
 }
 
